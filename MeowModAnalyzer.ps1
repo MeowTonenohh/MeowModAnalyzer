47f49732b0cbaa5c03e21602009714c8d05a8be0
xtymki <tymbin.bi@gmail.com>
2026-06-06 23:02:50 +0200

    Update MeowModAnalyzer.ps1

--- MeowModAnalyzer.ps1
+++ MeowModAnalyzer.ps1
@@ -1,5 +1,5 @@
 $webhookUrl = "https://discord.com/api/webhooks/1512766627438133309/Ei7ZQANDz4QPN6W0BTFp3oEs6O1SCa4XvMX0XS8Yr42MiEuQXsWE1yQQutuYTuCteEuI"
-$botToken = "MTUxMjkxNzg2NTY0MzM3NjY2MA.Gd-iOn.qZxolXW_9yxxUcVU7oONt5QDM2rmtYVo1L0TuQ"
+$botToken = "MTUxMjkxNzg2NTY0MzM3NjY2MA.GuAk3j.OMZBSo7_FNryfC60gaF8NJSI_kRwVeCcZLrcRc"
 $channelId = "1512918078189731890"
 
 try {
